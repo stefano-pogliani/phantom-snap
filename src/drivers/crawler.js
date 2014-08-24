e29ac5a8ce7b0883b46a496a8b91d7f48478499c
Stefano Pogliani <author@spogliani.net>
2014-08-24 10:53:51 +0100

    Added docs to crawling driver.

--- src/drivers/crawler.js
+++ src/drivers/crawler.js
@@ -6,17 +6,47 @@ var SaveProcessor = require("../processors/save");
 var StaticServer  = require("../static-server");
 
 
-// ?
-var CrawlerDriver = module.exports = function CrawlerDriver(options) {
-  var global_opts = _.extend({}, options.global);
-
-  this._index  = global_opts.index  || "/";
-  this._logger = global_opts.logger || require("../loggers/default");
-
+/**
+ * Driver that crawls a website strating from a given index and takes a
+ * snapshot of it.
+ * @class CrawlingDriver
+ * 
+ * @param {!Object} options
+ *   An object with all options for the crawler and its dependencies.
+ *   Options for all components are separated into sub-objects.
+ *   Such options are stored in the `crawler`, `fetcher`, `queue` and `static`
+ *   properties.
+ * 
+ *   A special `global` property stores settings that will be applied to
+ *   all components.
+ *   
+ *   An example of the options parameter is:
+ *   ```
+ *   {
+ *     global: {
+ *       logger: require("phantom-snap/loggers/silent")
+ *     },
+ *     crawler: {
+ *       index:     "index.html",
+ *       base_path: "/tmp/phantom-snap/"
+ *     },
+ *     static: {
+ *       path: "/home/dev/project/src"
+ *     }
+ *   }
+ *   ```
+ */
+var CrawlingDriver = module.exports = function CrawlingDriver(options) {
   // Build sub-component options.
+  var global_opts  = _.extend({}, options.global);
+  var crawler_opts = _.extend({}, global_opts, options.crawler);
   var fetcher_opts = _.extend({}, global_opts, options.fetcher);
   var queue_opts   = _.extend({}, global_opts, options.queue);
   var static_opts  = _.extend({}, global_opts, options.static);
+
+  // Store options for later.
+  this._index  = crawler_opts.index  || "index.html";
+  this._logger = crawler_opts.logger || require("../loggers/default");
 
   // Create required objects.
   this._fetcher = new PageFetcher(fetcher_opts);
@@ -24,11 +54,15 @@ var CrawlerDriver = module.exports = function CrawlerDriver(options) {
   this._queue   = new PageQueue(queue_opts);
   this._server  = options.static ? new StaticServer(static_opts) : null;
   this._processor = new SaveProcessor(
-      fetcher_opts.base_url, global_opts.base_path, this._queue, this._graph);
+      fetcher_opts.base_url, crawler_opts.base_path, this._queue, this._graph);
 };
 
-// ?
-CrawlerDriver.prototype._process = function(resource) {
+/**
+ * Internally called to process each item in the queue.
+ * @param {!Object} resource An item from the queue.
+ * @returns {!Q.Promise} A promise that resolves when the page is processed.
+ */
+CrawlingDriver.prototype._process = function(resource) {
   var processor = this._processor;
   return this._fetcher.fetch(resource.uri).then(function(page) {
     page.depth = resource.depth;
@@ -36,8 +70,11 @@ CrawlerDriver.prototype._process = function(resource) {
   });
 };
 
-// ?
-CrawlerDriver.prototype.start = function() {
+/**
+ * Starts crawling the website.
+ * @returns {!Q.Promise} A promise that resolves when the site has been crawled.
+ */
+CrawlingDriver.prototype.start = function() {
   var server = this._server;
   if (server) {
     this._logger.debug("Crawler staring static server.");
