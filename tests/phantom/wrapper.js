75018dd230e5eef4cacf86db424530368fcfa3e0
Stefano Pogliani <author@spogliani.net>
2014-08-24 14:18:07 +0100

    Refactoring tests.

--- tests/phantom/wrapper.js
+++ tests/phantom/wrapper.js
@@ -9,62 +9,6 @@ suite("PhantomJS Wrapper", function() {
     });
   });
 
-  test("failing fetch", function(done) {
-    this.timeout(4000);
-    var phantom = this.phantom;
-    var result  = null;
-
-    phantom.spawn().then(function() {
-      return phantom.fetch("non-existent-url", "");
-
-    }).then(function() {
-      // Verify Phantom fails.
-      result = new Error("Page fetch did not fail.");
-
-    }).fail(function(status) {
-      // Verify expected result.
-      assert.equal("fail", status);
-
-    }).fail(function(ex) {
-      // Intercept assert failures.
-      result = ex;
-
-    }).finally(function() {
-      // Stop Phantom test and resolve test.
-      phantom.stop().then(function() {
-        done(result);
-      });
-    });
-  });
-
-  test("fetch", function(done) {
-    this.timeout(4000);
-    var phantom = this.phantom;
-    var result  = null;
-
-    phantom.spawn().then(function() {
-      return phantom.fetch("https://www.google.com", "", {
-        path: "./waiters/noop"
-      });
-
-    }).then(function(page) {
-      // Verify results.
-      assert.equal("Google", page.title);
-      assert(typeof page._phantom_id === "number");
-      assert(page.phantom !== null);
-
-    }).fail(function(ex) {
-      // Intercept assert failures.
-      result = ex;
-
-    }).finally(function() {
-      // Stop Phantom test and resolve test.
-      phantom.stop().then(function() {
-        done(result);
-      });
-    });
-  });
-
   test("spawn", function(done) {
     var phantom = this.phantom;
     phantom.spawn().then(function() {
@@ -82,5 +26,64 @@ suite("PhantomJS Wrapper", function() {
       done(ex);
     });
   });
+});
 
+
+suite("PhantomJS Wrapper > pre-spawn", function() {
+  setup(function(done) {
+    this.phantom = new Phantom({
+      logger: require("../../out/loggers/silent")
+    });
+
+    this.phantom.spawn().then(function() {
+      done();
+    }).fail(function(ex) {
+      done(ex);
+    });
+  });
+
+  teardown(function(done) {
+    this.phantom.stop().then(function() {
+      done();
+    }).fail(function(ex) {
+      done(ex);
+    });
+  });
+
+  test("failing fetch", function(done) {
+    this.timeout(4000);
+    this.phantom.fetch("non-existent-url", "").then(function() {
+      // Verify Phantom fails.
+      throw new Error("Page fetch did not fail.");
+
+    }).fail(function(err) {
+      // Verify expected result.
+      if (typeof err !== "string") {
+        throw err;
+      }
+      assert.equal("fail", err);
+      done();
+
+    }).fail(function(ex) {
+      // Intercept assert failures.
+      done(ex);
+    });
+  });
+
+  test("fetch", function(done) {
+    this.timeout(4000);
+    this.phantom.fetch("https://www.google.com", "", {
+      path: "./waiters/noop"
+    }).then(function(page) {
+      // Verify results.
+      assert.equal("Google", page.title);
+      assert(typeof page._phantom_id === "number");
+      assert(page.phantom !== null);
+      done();
+
+    }).fail(function(ex) {
+      // Intercept assert failures.
+      done(ex);
+    });
+  });
 });
